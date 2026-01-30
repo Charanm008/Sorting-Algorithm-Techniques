4ef6122f60ff2087d46db74aa30aaf816b411324
Charan Manthena <charanmant8@gmail.com>
2026-01-30 11:57:04 +0530

    Add files via upload

---
+++ SelectionSort.java
@@ -0,0 +1,32 @@
+public class SelectionSort {
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIdx = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIdx]) {
+                    minIdx = j;
+                }
+            }
+            int temp = arr[minIdx];
+            arr[minIdx] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 5, 3, 8, 4, 2 };
+        System.out.println("Original array: ");
+        printArray(arr);
+        selectionSort(arr);
+        System.out.println("Sorted array: ");
+        printArray(arr);
+    }
+}
